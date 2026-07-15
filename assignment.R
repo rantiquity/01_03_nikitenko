6f13d3bc6d56f980b5ec2948893c224d881f1817
yulia-nikitenko86 <yulianikitenko86@gmail.com>
2026-07-15 19:51:50 +0100

    Update assignment.R

--- assignment.R
+++ assignment.R
@@ -10,11 +10,47 @@ meta <- oldFrenchMeta
 # показывающую распределение произведений по темам; цветом-заливкой закодируйте жанр; 
 meta |> 
   # ваш код здесь
+ggplot(aes(Genre, fill = Genre)) +
+geom_bar()
   # уберите названия осей; добавьте заголовок "Old French Data"
   # ваш код здесь 
+meta |> 
+ggplot(aes(Genre, fill = Genre)) +
+geom_bar() +
+labs(
+    title = "Old French Data",
+    x = NULL,
+    y = NULL
+     )
   # поверните координатную ось; 
   # ваш код здесь
+meta |> 
+ggplot(aes(y = Genre, fill = Genre)) +
+geom_bar() +
+labs(
+    title = "Old French Data",
+    x = NULL,
+    y = NULL
+     )
   # поменяйте тему оформления на черно-белую (bw) 
+meta |> 
+ggplot(aes(y = Genre, fill = Genre)) +
+geom_bar() +
+labs(
+    title = "Old French Data",
+    x = NULL,
+    y = NULL
+     ) +
+  theme_bw()
 
 # !!!! сохраните график как объект в окружении под именем g
 # !!!!вызов class(g) должен возвращать "gg"     "ggplot"
+g <- meta |> 
+ggplot(aes(y = Genre, fill = Genre)) +
+geom_bar() +
+labs(
+    title = "Old French Data",
+    x = NULL,
+    y = NULL
+     ) +
+  theme_bw()
